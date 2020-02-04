b1232a7bb98e6c9da0dd2e168cebf6771c76921e
Tenyo <THEG4ME@abv.bg>
2020-02-05 01:14:18 +0200

    up

---
+++ Exam Preparation - май 2019/src/Apps/Panda/Panda.Web/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using SIS.MvcFramework;
+using SIS.MvcFramework.Result;
+
+namespace Panda.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
