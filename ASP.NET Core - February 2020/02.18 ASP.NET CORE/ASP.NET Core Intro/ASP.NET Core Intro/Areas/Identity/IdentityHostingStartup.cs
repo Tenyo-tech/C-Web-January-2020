8a82fe2b8b2ca78276013831274b4bb72a66c961
Tenyo <THEG4ME@abv.bg>
2020-03-19 15:43:28 +0200

    update

---
+++ ASP.NET Core - February 2020/02.18 ASP.NET CORE/ASP.NET Core Intro/ASP.NET Core Intro/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ASP.NET_Core_Intro.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ASP.NET_Core_Intro.Areas.Identity.IdentityHostingStartup))]
+namespace ASP.NET_Core_Intro.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
